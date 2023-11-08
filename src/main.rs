4218bf8607dd47fa6a9941bdc6b6d740e990f724
Euna Islam <euna.islam@gmail.com>
2023-11-08 17:26:51 +0200

    Added sysinfo crate

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,41 @@
-use std::process::Command;
+extern crate sysinfo;
+
+use sysinfo::{ProcessExt, System, SystemExt};
 
 fn main() {
-    list_processes();
+    get_system_data();
 }
 
-fn list_processes() {
-    // Run a command to list all processes (e.g., using the `ps` command on Unix-like systems).
-    let output = Command::new("ps")
-    .arg("aux")
-    .output()
-    .expect("Failed to execute command");
+fn get_system_data() {
+    let mut sys = System::new_all();
 
-    // Print the output of the command (list of processes).
-    println!("Process List:\n{}", String::from_utf8_lossy(&output.stdout));
+    // First we update all information of our `System` struct.
+    sys.refresh_all();
+
+    // We display all disks' information:
+    println!("=> disks:");
+    for disk in sys.disks() {
+        println!("{:?}", disk);
+    }
+
+    println!("=> system:");
+    // RAM and swap information:
+    println!("total memory: {} bytes", sys.total_memory());
+    println!("used memory : {} bytes", sys.used_memory());
+    println!("total swap  : {} bytes", sys.total_swap());
+    println!("used swap   : {} bytes", sys.used_swap());
+
+    // Display system information:
+    println!("System name:             {:?}", sys.name());
+    println!("System kernel version:   {:?}", sys.kernel_version());
+    println!("System OS version:       {:?}", sys.os_version());
+    println!("System host name:        {:?}", sys.host_name());
+
+    // Number of CPUs:
+    println!("NB CPUs: {}", sys.cpus().len());
+
+    // Display processes ID, name na disk usage:
+    for (pid, process) in sys.processes() {
+        println!("[{}] {} {:?}", pid, process.name(), process.disk_usage());
+    }
 }
