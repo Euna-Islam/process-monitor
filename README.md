97a08477761b262bef10be9e823877edc0355d52
Euna Islam <euna.islam@gmail.com>
2023-11-15 14:35:39 +0200

    Working on command line argument
    working on command line arguments

--- README.md
+++ README.md
@@ -9,3 +9,6 @@
 [1] [Learn Rust Programming - Complete Course. FreeCodeCamp.](https://www.youtube.com/watch?v=BpPEoZW5IiY)<br>
 [2] [Crate sysinfo](https://docs.rs/sysinfo/latest/sysinfo/)<br>
 [3] [Rust Iterator Trait](https://doc.rust-lang.org/std/iter/trait.Iterator.html)<br>
+[4] [Rust Program Arguments](https://doc.rust-lang.org/rust-by-example/std_misc/arg.html)<br>
+[5] [Rust Flow of Control - Match](https://doc.rust-lang.org/rust-by-example/flow_control/match.html)<br>
+
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,29 @@ mod utils{
     pub mod process;
 }
 
+mod app_args;
+
+use std::env;
 use utils::system::{get_system_info, SystemInfo};
 use utils::process::{get_process_info, ProcessInfo};
 
 fn main() {
-    print_system_info();
-    print_process_info();
+    take_cmd_line_arg();
+}
+
+fn take_cmd_line_arg() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 1 {
+        match args[1].as_str() {
+            app_args::SYSTEM_ARG => print_system_info(),
+            app_args::PROCESS_ARG => print_process_info(),
+            app_args::HELP_ARG => println!("Not implemented yet!"),
+            _ => println!("Invalid argument. Use --system, or --process."),
+        }
+    } else {
+        println!("Please provide an argument: --system, or --process.");
+    }
 }
 
 fn print_process_info() {
