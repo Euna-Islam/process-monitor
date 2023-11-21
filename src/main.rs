66b5b49ff23c895f1b1fe8bf053e9fba92c0c909
Euna Islam <euna.islam@gmail.com>
2023-11-21 11:19:14 +0200

    implemented file reading

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ mod utils{
 }
 
 mod app_args;
-
 use std::env;
 use process_monitor::models::system_info::SystemInfo;
 use process_monitor::models::process_info::ProcessInfo;
@@ -13,21 +12,29 @@ use utils::system::get_system_info;
 use utils::process::get_process_info;
 
 fn main() {
-    take_cmd_line_arg();
+    process_cmd_line_arg();
 }
 
-fn take_cmd_line_arg() {
+fn process_cmd_line_arg() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() > 1 {
         match args[1].as_str() {
             app_args::SYSTEM_ARG => print_system_info(),
             app_args::PROCESS_ARG => print_process_info(),
-            app_args::HELP_ARG => println!("Not implemented yet!"),
-            _ => println!("Invalid argument. Use --system, or --process."),
+            app_args::HELP_ARG => print_help_file(),
+            _ => eprintln!("Invalid argument. Use --system, or --process."),
         }
     } else {
-        println!("Please provide an argument: --system, or --process.");
+        eprintln!("Please provide an argument: --help, --system, or --process.");
+    }
+}
+
+fn print_help_file() {
+    let result = std::fs::read_to_string("help.txt");
+    match result {
+        Ok(content) => { println!("File content: {}", content); }
+        Err(error) => { eprintln!("Internal error! Error: {}", error); }
     }
 }
 
--- src/test.js
+++
@@ -1,5 +0,0 @@
-function changeSorting(select) {
-    console.log("sorting");
-    let selectedOption = select.value;
-    window.location.href = `?sort=${selectedOption}`;
-}
