1e3cb1c8c5edbd72aec3d4c1db68cb695a1325e4
Euna Islam <euna.islam@gmail.com>
2023-11-21 10:40:25 +0200

    Tested clap

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ mod utils{
 
 mod app_args;
 
+use clap::Parser;
+
 use std::env;
 use process_monitor::models::system_info::SystemInfo;
 use process_monitor::models::process_info::ProcessInfo;
@@ -12,22 +14,50 @@ use process_monitor::models::process_info::ProcessInfo;
 use utils::system::get_system_info;
 use utils::process::get_process_info;
 
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    path: std::path::PathBuf,
+}
+
 fn main() {
     take_cmd_line_arg();
 }
 
 fn take_cmd_line_arg() {
-    let args: Vec<String> = env::args().collect();
 
-    if args.len() > 1 {
-        match args[1].as_str() {
-            app_args::SYSTEM_ARG => print_system_info(),
-            app_args::PROCESS_ARG => print_process_info(),
-            app_args::HELP_ARG => println!("Not implemented yet!"),
-            _ => println!("Invalid argument. Use --system, or --process."),
-        }
-    } else {
-        println!("Please provide an argument: --system, or --process.");
+    let args: Cli = Cli::parse();
+
+    println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
+
+    let my_c_instance = C {}; // Creating an instance of struct C
+
+    my_c_instance.print(&args);
+    // let args: Vec<String> = env::args().collect();
+
+    // if args.len() > 1 {
+    //     match args[1].as_str() {
+    //         app_args::SYSTEM_ARG => print_system_info(),
+    //         app_args::PROCESS_ARG => print_process_info(),
+    //         app_args::HELP_ARG => println!("Not implemented yet!"),
+    //         _ => println!("Invalid argument. Use --system, or --process."),
+    //     }
+    // } else {
+    //     println!("Please provide an argument: --system, or --process.");
+    // }
+}
+
+struct C {}
+
+impl C {
+    // Method that accepts an instance of struct X
+    fn print(&self, x: &Cli) {
+        // Print or perform operations on the received X instance
+        println!("Received data: {:?}", x.pattern);
+        println!("Received file path: {:?}", x.path);
+        // Additional processing or operations can be performed here
     }
 }
 
